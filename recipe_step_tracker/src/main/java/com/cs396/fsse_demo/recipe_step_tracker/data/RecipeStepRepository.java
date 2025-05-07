0c0f8966e2fdb22686275daad7525f27e4d2b945
Lydia Tse <lydia.tse@northwestern.edu>
2025-05-07 13:22:56 -0500

    Delete recipe_step_tracker directory

--- recipe_step_tracker/src/main/java/com/cs396/fsse_demo/recipe_step_tracker/data/RecipeStepRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cs396.fsse_demo.recipe_step_tracker.data;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeStepRepository extends CrudRepository<RecipeStep, Long> {
-}
